994db6283cd2ae071e21cadbbcbdd0886f55c1ef
Troy Arnold <troylarnold@gmail.com>
2022-05-23 16:15:48 -0700

    Bootcamp stuff you already know

---
+++ objects_and_datatypes/main.py
@@ -0,0 +1,37 @@
+def integers():
+
+    addition = 2 + 1
+    print(addition)
+
+    subtraction = 5 - 1
+    print(subtraction)
+
+    modulo = 5 % 3
+    print(modulo)
+
+    multiplication = 2 * 2
+    print(multiplication)
+
+    division = 9 / 5
+    print(division)
+
+    four_squared = 4 ** 2
+    print(four_squared)
+
+def variable_assignment():
+    cat = "cat"
+    a = 7
+
+    print(a * 5)
+    print((cat + " ") * 5)
+
+def strings():
+    #start, stop, step
+
+    hello = "hello world"
+    random_string = "Hello \nmy \nname \nis \nTroy."
+    print(hello)
+    print(random_string)
+
+if __name__ == "__main__":
+    strings()
