bbad131ad9b917597c72198c21453100adef35f3
Troy Arnold <troylarnold@gmail.com>
2022-05-23 16:36:55 -0700

    Lists refresher

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 <h1>2022 Python Bootcamp</h1>
+<p>A lot of this will be a refresher, but hopefully some of it will be new! </p>
+<p>Following PEP8 style throughout.</p>
--- objects_and_datatypes/main.py
+++ objects_and_datatypes/main.py
@@ -26,12 +26,45 @@ def variable_assignment():
     print((cat + " ") * 5)
 
 def strings():
-    #start, stop, step
+    #strings are immutable
+    #start, stop, step for slicing
 
     hello = "hello world"
     random_string = "Hello \nmy \nname \nis \nTroy."
     print(hello)
     print(random_string)
 
+    print(hello[0])
+    print(hello[:3])
+    print(hello[5:])
+
+    lower = 'troy'
+    upper = 'TROY'
+
+    print(f'{lower} is lowercase until we call upper(), then it is {lower.upper()}.')
+    print(f'{upper} is uppercase until we call lower(), then it is {upper.lower()}.')
+
+    print(hello.split())
+
+def lists():
+    my_list = ['A',7,'bee', 'jam']
+    print(my_list)
+
+    my_list.append("Troy")
+    print(my_list)
+
+    my_list.pop()
+    print(my_list)
+
+    item = my_list.pop(2)
+    print(item)
+    print(my_list)
+
+    new_list = [1, 9, 6, 15, 2]
+    new_list.sort() # occurs in place
+    print(new_list)
+    new_list.reverse()
+    print(new_list)
+
+
 if __name__ == "__main__":
-    strings()
