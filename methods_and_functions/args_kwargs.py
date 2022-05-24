93373a1f91adf2be6f16f55330c9dd41bf636098
Troy Arnold <troylarnold@gmail.com>
2022-05-24 16:51:35 -0700

    Easy excersize

--- methods_and_functions/args_kwargs.py
+++ methods_and_functions/args_kwargs.py
@@ -13,5 +13,22 @@ def arg_kwarg(*args, **kwargs):
     print(kwargs)
     print(f'I would like {args[0]} {kwargs["food"]}.')
 
+def pick_even(*args):
+    evens = []
+    for number in args:
+        if number % 2 == 0:
+            evens.append(number)
+    return evens
+
+def skyline(word):
+    sky = ''
+    for i in range(0, len(word)):
+        if i % 2 == 0:
+            sky += word[i].upper()
+        else:
+            sky += word[i].lower()
+
+    return sky
+
 if __name__ == "__main__":
-    arg_kwarg(10,20,30,fruit='orange',bat='yes',food='hotdog')
+    print(skyline('hamster'))
