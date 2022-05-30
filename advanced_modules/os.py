63801eb2a4e124f0911e0a588ec926d80754d11a
Troy Arnold <troylarnold@gmail.com>
2022-05-30 12:02:14 -0700

    OS module

--- advanced_modules/os.py
+++ advanced_modules/os.py
@@ -1,10 +1,26 @@
 import os
 import shutil
+import send2trash
 
 with open('practice.txt', 'w+') as f:
-    f.write('Test')
+    f.write('Test\n')
+    f.write('Hotdog')
     f.close()
 
+# Most likely permanent removal
+# os.unlink(path)
+# os.rmdir(path)
+# shutil.rmtree(path) # dangerous removal method if mistakes, beware
+
+# removal method that sends to trash, less permanent
+#send2trash.send2trash('practice.txt')
+
+for folder, sub_folders, files in os.walk(os.getcwd()):
+    print(folder)
+    print(sub_folders)
+    print(files)
+
 if __name__ == "__main__":
-    print(os.getcwd())
-    print(os.listdir())
+    pass
+    #print(os.getcwd())
+    #print(os.listdir())
