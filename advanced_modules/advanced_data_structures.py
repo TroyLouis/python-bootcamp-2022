a0730db89dba6b147db60415304e3c8c8b42d880
Troy Arnold <troylarnold@gmail.com>
2022-06-02 20:31:32 -0700

    Advanced structures

---
+++ advanced_modules/advanced_data_structures.py
@@ -0,0 +1,21 @@
+print(hex(12))
+print(hex(512))
+print(bin(512))
+print(pow(2,6))
+
+# (x ^ y) % z
+print(pow(2,6,2))
+
+print(abs(-15))
+
+# rounds to 0 decimal
+print(type(round(3.50)))
+
+print(round(3.141592,2))
+
+x = [1,2,3,]
+x.extend([4,5])
+print(x)
+
+if __name__ == "__main__":
+    pass
